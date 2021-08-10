07ae911359784882d40525853689536b016318ab
SatyaDivya <divya.maddipudi1@gmail.com>
2021-08-11 00:41:53 +0530

    Tinder-clone added

---
+++ ReactProjects/typing-test/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Type from './Type';
+
+function App() {
+  return (
+    <div className="App">
+      <Type />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactProjects/typing-test/src/Type.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Type.css";
+
+function Type() {
+  return (
+    <div className="type">
+      <div className="type__link">
+      <a href="">typing-test</a>
+      </div>
+      <div className="type__time">
+        <h4>
+          time: 
+          
+          
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default Type;
